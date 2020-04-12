30e409b70687a8fd48e7e00c45fa43a189e36e1b
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-12 23:36:32 +0300

    added dynamic styles to main-app

--- main-app/src/app/app.module.ts
+++ main-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponent,
     ServersComponent,
   ],
   imports: [
--- main-app/src/app/servers/servers.component.ts
+++ main-app/src/app/servers/servers.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
-  serverName = ''
+export class ServersComponent implements OnInit {
   allowNewServer = false
+  serverCreationStatus = 'No server was created!'
+  serverName = 'Testserver'
+  serverCreated = false
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 3000)
+  }
+
+  ngOnInit() { }
 
   onCreateServer() {
-    
+    this.serverCreated = true
+    this.serverCreationStatus = 'Server was created! The name of the server is ' + this.serverName
   }
 }
