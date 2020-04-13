b6bcde5bddf550f30785438e02a22bc6bc447a7f
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-13 17:49:05 +0300

    added directives to main-app

--- main-app/src/app/server/server.component.ts
+++ main-app/src/app/server/server.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white
+    }
+  `]
 })
 export class ServerComponent {
-  serverId: 10
+  serverId: number = 10
   serverStatus: string = 'offline'
 
   constructor() {
