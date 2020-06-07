203c1eb1f0d08d306127f1212733a1f7247794d3
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-06-07 19:35:18 +0300

    observing responses

--- http/src/app/posts.service.ts
+++ http/src/app/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators'
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
+import { map, catchError, tap } from 'rxjs/operators'
 import { Subject, throwError } from 'rxjs';
 
 import { Post } from './post.model';
@@ -15,7 +15,10 @@ export class PostsService {
     const postData: Post = {title: title, content: content}
     this.http.post<{ name: string }> (
       'https://ng-complete-guide-bb45e.firebaseio.com/posts.json',
-      postData
+      postData,
+      {
+        observe: 'response'
+      }
     ).subscribe(responseData => {
       console.log(responseData)
     }, error => {
@@ -53,6 +56,19 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete('https://ng-complete-guide-bb45e.firebaseio.com/posts.json')
+    return this.http.delete(
+      'https://ng-complete-guide-bb45e.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      }
+    ).pipe(tap(event => {
+      console.log(event);
+      if (event.type === HttpEventType.Sent) {
+        // ...
+      }
+      if (event.type === HttpEventType.Response) {
+        console.log(event.body);
+      }
+    }))
   }
 }
