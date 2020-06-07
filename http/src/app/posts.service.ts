3a5e83c9df6fd0d8fd6fabb67abb67f19a760dc6
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-06-07 18:25:10 +0300

    added query params

--- http/src/app/posts.service.ts
+++ http/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators'
 import { Subject, throwError } from 'rxjs';
 
@@ -24,28 +24,32 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams()
+    searchParams = searchParams.append('print', 'pretty')
+    searchParams = searchParams.append('custom', 'key')
     return this.http
-    .get<{ [key: string]: Post }>(
-      'https://ng-complete-guide-bb45e.firebaseio.com/posts.json',
-      {
-        headers: new HttpHeaders({"'Custom-Header': 'Hello"})
-      }
-    )
-    .pipe(
-      map((responseData: {[key: string]: Post}) => {
-        const postsArray: Post[] = []
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postsArray.push({...responseData[key], id: key})
-          }
+      .get<{ [key: string]: Post }>(
+        'https://ng-complete-guide-bb45e.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+          params: searchParams
         }
-        return postsArray
-      }),
-      catchError(errorRes => {
-        // Send to analytics server
-        return throwError(errorRes)
-      })
-    )
+      )
+      .pipe(
+        map((responseData: {[key: string]: Post}) => {
+          const postsArray: Post[] = []
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key], id: key})
+            }
+          }
+          return postsArray
+        }),
+        catchError(errorRes => {
+          // Send to analytics server
+          return throwError(errorRes)
+        })
+      )
   }
 
   deletePosts() {
